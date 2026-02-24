ebda6270ddd5365937ea7fadbc47ed8900f2d121
vtu25055-lgtm <vtu25055@veltech.edu.in>
2026-02-24 23:02:07 +0530

    Time: 4 ms (73.48%), Space: 45.7 MB (9.68%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        // Step 1: Process nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        // Remaining elements have no next greater
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+        
+        // Step 2: Build result for nums1
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.get(nums1[i]);
+        }
+        
+        return result;
+    }
+}
