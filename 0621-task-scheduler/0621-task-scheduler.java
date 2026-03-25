4e9400929aac1791c81a0bb85904ee76a7850903
vtu25055-lgtm <vtu25055@veltech.edu.in>
2026-03-25 09:20:16 +0530

    Time: 2 ms (98.37%), Space: 48.4 MB (30.72%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        int maxFreq = 0;
+        for (int f : freq) {
+            maxFreq = Math.max(maxFreq, f);
+        }
+
+        int countMax = 0;
+        for (int f : freq) {
+            if (f == maxFreq) {
+                countMax++;
+            }
+        }
+
+        int partCount = maxFreq - 1;
+        int partLength = n + 1;
+        int emptySlots = partCount * partLength + countMax;
+
+        return Math.max(emptySlots, tasks.length);
+    }
+}
